30242668ea1989907e509c6c4a7918d8a3426257
unknown <talkwithricii15@gmail.com>
2026-08-29 16:18:52 +0800

    Fix GitHub Pages routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
@@ -39,7 +40,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
 
       <div className="App">
 
@@ -128,7 +129,7 @@ function App() {
 
       </div>
 
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
